f7b7f5a5f2885db9714bba47aba43ad433c728a5
JhonPuentes01 <jhon.puentes@qcode.co>
2024-07-05 08:55:25 -0500

    middleware!

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,24 +1,18 @@
 <?php
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\TimesheetController;
+use App\Http\Controllers\ClientController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('employees', EmployeeController::class);
+    Route::apiResource('timesheets', TimesheetController::class);
+    Route::apiResource('clients', ClientController::class);
 
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+});
