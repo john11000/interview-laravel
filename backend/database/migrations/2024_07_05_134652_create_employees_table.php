657d8b8deb34da84c2fe2c8eac005bc6115e296f
JhonPuentes01 <jhon.puentes@qcode.co>
2024-07-05 08:51:17 -0500

    feat: auth laravel and tables

---
+++ backend/database/migrations/2024_07_05_134652_create_employees_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('payment_type'); // 'hourly' or 'salary'
+            $table->decimal('payment_amount', 8, 2);
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+};
